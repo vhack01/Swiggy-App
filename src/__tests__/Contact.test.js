0d0c7ee6b9f4a8537968e29f686ffd2a416f072b
vishwas <vishwas101kumar@gmail.com>
2024-06-07 15:40:17 +0530

    Done with unit testing of Contact component

--- src/__tests__/Contact.test.js
+++ src/__tests__/Contact.test.js
@@ -2,9 +2,29 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../components/Contact";
 import "@testing-library/jest-dom";
 
-test("Function to check About page is loading or not", () => {
-  render(<Contact />);
-  const heading = screen.getByRole("heading");
-  console.log("Screen:", screen);
-  expect(heading).toBeInTheDocument();
+describe("Contact Component", () => {
+  test("Function to check About page is loading or not", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("Checking submit button in Contact Component", () => {
+    render(<Contact />);
+    const buttonText = screen.getByText("Submit");
+  });
+
+  test("Checking input in Contact Component", () => {
+    render(<Contact />);
+    const inputName = screen.getByPlaceholderText("Enter name");
+    expect(inputName).toBeInTheDocument();
+  });
+
+  // Testing Multiple tag
+  it("Checking all input in Contact Component", () => {
+    render(<Contact />);
+    const textbox = screen.getAllByRole("textbox");
+    console.log("textbox:", textbox);
+    expect(textbox.length).toBe(1);
+  });
 });
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,14 @@
 const Contact = () => {
-  return <h1>Contact Us</h1>;
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <form>
+        <input type="text" placeholder="Enter name" name="fname" />
+        <input type="password" placeholder="Enter password" name="pass" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
